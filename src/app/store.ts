a9cae4e2d948d2986fbbc3dd8e34aea6e3374f52
Kheyzell <kheyzell@gmail.com>
2021-05-18 13:34:49 +0200

    fix: removed unused Counter store slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import techsReducer from '../features/tech-tree/techSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     techs: techsReducer,
   },
 });
