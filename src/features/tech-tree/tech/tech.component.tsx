b60aab20e87db9c71d433fcf83808f1fa3431b2e
Kheyzell <kheyzell@gmail.com>
2021-05-18 19:54:39 +0200

    feature: added selected list of techs in the tech tree tool (+ civ panel background cover)

--- src/features/tech-tree/tech/tech.component.tsx
+++ src/features/tech-tree/tech/tech.component.tsx
@@ -2,12 +2,18 @@ import React from "react"
 import { useDispatch, useSelector } from "react-redux"
 
 import { Tech, TechType } from "../../../models/techs.model"
-import { selectedTechsSelector, selectTech, toggleTechsSelelection } from "../techSlice"
+import { selectedTechsSelector, toggleTechsSelelection } from "../techSlice"
 
 import './tech.component.css'
 
+export enum BoxSize {
+  mini = 'mini',
+  normal = 'normal'
+} 
+
 type Props = {
   tech: Tech
+  size?: BoxSize
   onTechSelected?(tech: Tech): void
 }
 type State = {}
@@ -22,9 +28,12 @@ const TechComponent: React.FC<Props> = (props, state: State) => {
     dispatch(toggleTechsSelelection({ ...props.tech }))
   }
 
+  const size = props.size || BoxSize.normal
+
   const techClass = props.tech.type === TechType.unit ? 'UnitTech' : 'UpgradeTech'
+  const sizeClass = size === BoxSize.normal ? 'Normal' : size === BoxSize.mini ? 'Mini' : ''
   return (
-    <div className={`Tech ${techClass} ${isSelected() ? 'Selected' : ''} ${props.tech.unique ? 'Unique' : ''}`} onClick={onTechClick}>
+    <div className={`Tech ${techClass} ${sizeClass} ${isSelected() ? 'Selected' : ''} ${props.tech.unique ? 'Unique' : ''}`} onClick={onTechClick}>
       <span className="Name"> {props.tech.name} </span>
       <img src={'./' + process.env.PUBLIC_URL + '/images/techs/' + props.tech.id + '.png'} />
     </div>
