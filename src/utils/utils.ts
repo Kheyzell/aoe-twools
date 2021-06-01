eb033661be1ea7f8cdbb2b7028467a8e2253624b
Kheyzell <kheyzell@gmail.com>
2021-06-01 23:14:20 +0200

    feature: Civ List: added horizontal scroll

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,3 +3,17 @@ export const stopEventPropagation = (event: React.MouseEvent<HTMLElement>) => {
     event.stopPropagation()
     event.nativeEvent.stopImmediatePropagation()
 }
+
+const wheelSpeed = 3;
+export const scrollHorizontally = (e: any, scrollRef: React.RefObject<HTMLElement>) => {
+  e.preventDefault();
+  if (scrollRef && scrollRef.current) {
+    const container = scrollRef.current
+    const containerScrollPosition = scrollRef.current.scrollLeft
+
+    container.scrollTo({
+      top: 0,
+      left: containerScrollPosition + e.deltaY * wheelSpeed,
+    })
+  }
+}
