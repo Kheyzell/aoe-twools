df3fc7c7980af0ede110a5349c2a25824957224f
Kheyzell <kheyzell@gmail.com>
2021-06-04 14:52:32 +0200

    feature: Civ List: added civilisation with fully upgraded selected units from filter list option

--- src/features/civ-list/civ-list.tsx
+++ src/features/civ-list/civ-list.tsx
@@ -2,14 +2,14 @@ import React, { useRef } from "react"
 import { useDispatch, useSelector } from "react-redux"
 
 import { allCivTechTrees } from "../../constants"
-import { CivTechTree, Tech } from "../../models/techs.model"
-import { civHasTech } from "../../utils/tech-tree.utils"
+import { CivTechTree, TechType, Unit, Upgrade } from "../../models/techs.model"
+import { civHasUnit, civHasUpgrade } from "../../utils/tech-tree.utils"
 import { scrollHorizontally } from "../../utils/utils"
 import { selectedCiv2Selector, selectedCivSelector, selectedTechsSelector, toggleCiv2Selection, toggleCivSelection } from "../tech-tree/civFilterSlice"
 import './civ-list.css'
 import CivPanel from "./civ-panel"
 
-type ShowCivPanel = {[civId: string]: boolean}
+type ShowCivPanel = { [civId: string]: boolean }
 
 type Props = {}
 type State = {}
@@ -17,20 +17,13 @@ type State = {}
 const CivList: React.FC<Props> = (props) => {
     const dispatch = useDispatch()
     const filteredListRef = useRef<HTMLElement>(null)
+    const fullyUpgradedListRef = useRef<HTMLElement>(null)
     const excludedListRef = useRef<HTMLElement>(null)
 
     const allCivTechs: CivTechTree[] = [...allCivTechTrees]
     const selectedCiv = useSelector(selectedCivSelector)
     const selectedCiv2 = useSelector(selectedCiv2Selector)
     const selectedTechs = useSelector(selectedTechsSelector)
-
-    const filteredTechTrees = allCivTechs.filter(civTechs => {
-        return selectedTechs.reduce((hasAllTechs: Boolean, currentTech: Tech) => {
-            return hasAllTechs && civHasTech(civTechs, currentTech)
-        }, true)
-    })
-
-    const excludedTechTrees = allCivTechs.filter(civ => !filteredTechTrees.find(filteredCiv => filteredCiv.id === civ.id))
 
     const onCivClick = (civ: CivTechTree) => {
         dispatch(toggleCivSelection({ ...civ }))
@@ -43,6 +36,7 @@ const CivList: React.FC<Props> = (props) => {
     }
 
     const [showTools, setShowTools] = React.useState<boolean>(false)
+    const [showCivsFullyUpgraded, setShowCivsFullyUpgraded] = React.useState<boolean>(false)
     const [showCivsExcluded, setShowCivsExcluded] = React.useState<boolean>(false)
     const onEnterTools = () => setShowTools(true)
     const onLeaveTools = () => setShowTools(false)
@@ -57,16 +51,45 @@ const CivList: React.FC<Props> = (props) => {
         setShowCivPanels({})
     }
 
+
+    const selectedUnits = selectedTechs.filter(tech => tech.type === TechType.unit) as Unit[]
+    const civsWithSelectedUnits = allCivTechs.filter(civTechs => {
+        return selectedUnits.reduce((hasAllUnits: Boolean, currentUnit: Unit) => {
+            return hasAllUnits && civHasUnit(civTechs, currentUnit)
+        }, true)
+    })
+    
+    const filteredTechTrees = civsWithSelectedUnits.filter(civTechs => {
+        const selectedUpgrades = selectedTechs.filter(tech => tech.type === TechType.upgrade) as Upgrade[]
+        return selectedUpgrades.reduce((hasAllTechs: Boolean, currentUpgrade: Upgrade) => {
+            return hasAllTechs && civHasUpgrade(civTechs, currentUpgrade)
+        }, true)
+    })
+    const fullyUpgradedFilteredUnitsTechTrees = showCivsFullyUpgraded && selectedUnits.length > 0 ? civsWithSelectedUnits.filter(civ => {
+        const allNeededUpgrades = selectedUnits.map(unit => unit.affectingUpgrades || []).reduce((neededUpgrades: Upgrade[], unitAffectingUpgrades: Upgrade[]) => {
+            return neededUpgrades.concat(unitAffectingUpgrades)
+        }, [])
+        return allNeededUpgrades.reduce((hasAllUpgrades: boolean, upgrade: Upgrade) => {
+            return hasAllUpgrades && civHasUpgrade(civ, upgrade)
+        }, true)
+    }) : []
+    const excludedTechTrees = showCivsExcluded ? allCivTechs.filter(civ => !filteredTechTrees.find(filteredCiv => filteredCiv.id === civ.id)) : []
+
     return (
         <div className="CivList">
             <div className="Tools" onMouseEnter={() => onEnterTools()} onMouseLeave={() => onLeaveTools()}>
                 {
                     showTools ?
-                        (<label onClick={() => { setShowCivsExcluded(!showCivsExcluded); setShowTools(false) }}>
-                            <input type="checkbox" checked={showCivsExcluded} /> Show Excluded
-                        </label>)
+                        (<div>
+                            <label htmlFor="FullyUpgraded" onClick={() => { setShowCivsFullyUpgraded(!showCivsFullyUpgraded) }} title="Show the list of civilisations having the selected units fully upgraded">
+                                <input name="FullyUpgraded" type="checkbox" checked={showCivsFullyUpgraded} /> Show Fully upgraded
+                            </label>
+                            <label htmlFor="Excluded" onClick={() => { setShowCivsExcluded(!showCivsExcluded) }} title="Show the list of civilisations that do not satisfy the filters">
+                                <input name="Excluded" type="checkbox" checked={showCivsExcluded} /> Show Excluded
+                            </label>
+                        </div>)
                         :
-                        (<button> <span> &gt; </span> </button>)
+                        (<button> <span> &lt; </span> </button>)
                 }
 
             </div>
@@ -90,6 +113,29 @@ const CivList: React.FC<Props> = (props) => {
                 })}
             </div>
 
+            { showCivsFullyUpgraded ?
+                (
+                    <div className="List FullyUpgraded" ref={fullyUpgradedListRef as React.RefObject<HTMLDivElement>} onWheel={e => scrollHorizontally(e, fullyUpgradedListRef)}>
+                        {fullyUpgradedFilteredUnitsTechTrees.map(civ => {
+                            const isSelected1 = !!selectedCiv && selectedCiv.id === civ.id
+                            const isSelected2 = !!selectedCiv2 && selectedCiv2.id === civ.id
+                            return (
+                                <div className={`CivTree ${isSelected1 ? 'Selected1' : ''} ${isSelected2 ? 'Selected2' : ''}`}
+                                    key={civ.id}
+                                    onClick={() => onCivClick(civ)}
+                                    onContextMenu={e => onCiv2Click(e, civ)}
+                                    onMouseEnter={() => onEnterCivCrest(civ)}
+                                    onMouseLeave={() => onLeaveCivCrest()}>
+                                    <span> {civ.name} </span>
+                                    <img src={civ.crest} alt={civ.name} />
+                                    <CivPanel civ={civ} show={showCivPanels[civ.id]}></CivPanel>
+                                </div>
+                            )
+                        })}
+                    </div>
+                )
+                : ''}
+            
             { showCivsExcluded ?
                 (
                     <div className="List Excluded" ref={excludedListRef as React.RefObject<HTMLDivElement>} onWheel={e => scrollHorizontally(e, excludedListRef)}>
